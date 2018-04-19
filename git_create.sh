566809d917286286b7f97ef908c8a094c5afd132
Epillepsy <gueye_o@etna-alternance.net>
2018-04-19 17:20:38 +0200

    Update git_create.sh

--- git_create.sh
+++ git_create.sh
@@ -1,13 +1,11 @@
-function git-create()
-{
+function git-create() {
   repo_name=$1
-  username='username'
-  test -z mon_test2 && echo Repo name required. 1>&2 && return 1
-  curl -u $username https://api.github.com/user/repos -d {"name":"$repo_name"}
+  username='epillepsy'
+  test -z $repo_name && echo "Repo name required." 1>&2 && return 1
+  curl -u $username https://api.github.com/user/repos -d "{\"name\":\"$repo_name\"}"
   git init
-  git remote add origin https://github.com/$username/mon_test2.git
+  git remote add origin "https://github.com/$username/$repo_name.git"
   git add *
-  git commit -m auto creation of repository
+  git commit -m "auto creation of repository"
   git push --set-upstream origin master
-  return 0
 }
