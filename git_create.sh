5d3f247e489e9aaf83b1677dc71efaa0f0681d12
Epillepsy <gueye_o@etna-alternance.net>
2018-04-19 17:19:37 +0200

    Update git_create.sh

--- git_create.sh
+++ git_create.sh
@@ -3,9 +3,9 @@ function git-create()
   repo_name=$1
   username='username'
   test -z mon_test2 && echo Repo name required. 1>&2 && return 1
-  curl -u $username https://api.github.com/user/repos -d {"name":"mon_test2"}
+  curl -u $username https://api.github.com/user/repos -d {"name":"$repo_name"}
   git init
-  git remote add origin https://github.com/$repo_name/mon_test2.git
+  git remote add origin https://github.com/$username/mon_test2.git
   git add *
   git commit -m auto creation of repository
   git push --set-upstream origin master
